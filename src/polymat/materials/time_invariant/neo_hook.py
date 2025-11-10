de52459dd873e0577dd4f23ce14855287a93b756
Guillermo Giraldo <ggiraldo@simscale.com>
2025-11-10 18:10:21 -0500

    add neo hook material

---
+++ src/polymat/materials/time_invariant/neo_hook.py
@@ -0,0 +1,41 @@
+from numpy import dot, eye, trace
+from numpy.linalg import det
+
+from polymat.types import Tensor
+
+
+def NeoHook(F: Tensor, params: list[float]) -> Tensor:
+    """
+    Neo-Hookean hyperelastic material model.
+
+    3D loading specified by deformation gradient F.
+
+    Parameters
+    ----------
+    F: Tensor
+        Deformation gradient tensor of shape (3,3)
+
+    params: list[float]
+        Material parameters [mu, kappa]
+
+    Returns
+    -------
+    Stress: Tensor
+        Cauchy stress tensor.
+    """
+    mu: float = params[0]
+    kappa: float = params[1]
+
+    J: float = det(F)
+
+    Fstar: Tensor = J ** (-1.0 / 3.0) * F
+
+    bstar: Tensor = dot(Fstar, Fstar.T)
+
+    dev_bstar: Tensor = bstar - trace(bstar) / 3.0 * eye(3)
+
+    Stress_dev: Tensor = mu / J * dev_bstar
+
+    Stress_vol: Tensor = kappa * (J - 1.0) * eye(3)
+
+    return Stress_dev + Stress_vol
---
+++ test/materials/test_neohook.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from polymat.materials.time_invariant.neo_hook import NeoHook
+from polymat.mechanics.deformation import uniaxial_tension
+from polymat.types import Tensor, Vector
+
+
+def test_neo_hook_zero_strain() -> None:
+    test_mat: list[float] = [1.0, 100.0]
+
+    F: Tensor = np.eye(3)
+
+    Stress: Tensor = NeoHook(F, test_mat)
+
+    assert np.isclose(Stress[0, 0], 0.0, atol=1e-6)
+
+
+def test_neo_hook_uniaxial_tension() -> None:
+    test_mat: list[float] = [0.5838, 1e6]
+
+    trueStrain: Vector = np.linspace(0.0, np.log(7.0), 100)
+
+    trueStress: Vector = uniaxial_tension(NeoHook, trueStrain, test_mat)
+
+    assert np.isclose(trueStress[-1], 29.02, rtol=0.02)
