ad2855e622241cec3cd2cfe92a47c694e0467c7c
Guillermo Giraldo <ggiraldo@simscale.com>
2025-11-10 18:05:18 -0500

    Add mooney material

---
+++ src/polymat/materials/time_invariant/mooney.py
@@ -0,0 +1,57 @@
+from numpy import dot, eye, trace
+from numpy.linalg import det
+
+from polymat.types import Tensor
+
+
+def Mooney5(F: Tensor, params: list[float]) -> Tensor:
+    """
+    Mooney 5-terms polynomial hyperelastic material model.
+
+    3D loading specified by deformation gradient F.
+
+    Parameters
+    ----------
+    F: Tensor
+        Deformation gradient tensor as array of size (3x3)
+
+    params: list[float]
+        Material parameters [C10, C02, C11, C20, C30, kappa]
+
+    Returns
+    -------
+    Stress: Tensor
+        Cauchy stress tensor
+    """
+    C10: float = params[0]
+    C01: float = params[1]
+    C11: float = params[2]
+    C20: float = params[3]
+    C30: float = params[4]
+    kappa: float = params[5]
+
+    J: float = det(F)
+
+    Fstar: Tensor = J ** (-1.0 / 3.0) * F
+
+    bstar: Tensor = dot(Fstar, Fstar.T)
+
+    bstar2: Tensor = dot(bstar, bstar)
+
+    I1star: float = trace(bstar)
+
+    I2star: float = 0.5 * (I1star**2.0 - trace(bstar2))
+
+    dPhi_dI1star: float = C10 + C11 * (I2star - 3.0) + 2.0 * C20 * (I1star - 3.0) + 3.0 * C30 * (I1star - 3.0) ** 2.0
+
+    dPhi_dI2star: float = C01 + C11 * (I1star - 3.0)
+
+    Stress_dev: Tensor = 2.0 / J * (dPhi_dI1star + dPhi_dI2star * I1star) * bstar
+
+    Stress_dev += -2.0 / J * dPhi_dI2star * bstar2
+
+    Stress_dev += -2.0 / (3.0 * J) * (I1star * dPhi_dI1star + 2.0 * I2star * dPhi_dI2star) * eye(3)
+
+    Stress_vol: Tensor = 3.0 * kappa * (J - 1.0) * eye(3)
+
+    return Stress_dev + Stress_vol
---
+++ test/materials/test_mooney.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from polymat.materials.time_invariant.mooney import Mooney5
+from polymat.mechanics.deformation import uniaxial_tension
+from polymat.types import Tensor, Vector
+
+
+def test_mooney_zero_strain() -> None:
+    test_mat: list[float] = [0.175, 0.0, 0.0, -1.35e-3, 3.9e-5, 2e3]
+
+    F: Tensor = np.eye(3)
+
+    Stress: Tensor = Mooney5(F, test_mat)
+
+    assert np.isclose(Stress[0, 0], 0.0, atol=1e-6)
+
+
+def test_mooney_uniaxial_tension() -> None:
+    test_mat: list[float] = [0.175, 0.0, 0.0, -1.35e-3, 3.9e-5, 1e3]
+
+    trueStrain: Vector = np.linspace(0.0, np.log(7.0), 100)
+
+    trueStress: Vector = uniaxial_tension(Mooney5, trueStrain, test_mat)
+
+    assert np.isclose(trueStress[-1], 29.02, rtol=0.02)
