179d560c7332cefc415c8ff2890c53c5dbc6b2f1
Guille Giraldo <guillegiraldo@gmail.com>
2025-11-22 11:43:57 -0500

    improve solvers

--- test/calibration/test_solvers.py
+++ test/calibration/test_solvers.py
@@ -1,19 +1,20 @@
 from numpy import isclose, linspace
 from pytest import mark
 
-from polymat.calibration.solvers import single_test_elastic_simplex
+from polymat.calibration.solvers import fit_single_test_elastic, fit_single_test_incompressible
 from polymat.materials.time_invariant.yeoh import Yeoh
 from polymat.mechanics.elastic_deformation import uniaxial_stress
+from polymat.mechanics.incompressible_deformation import uniaxial_stress_incompressible
 from polymat.types import Vector
 
 
 @mark.xfail
-def test_single_test_elastic_simplex() -> None:
+def test_fit_single_test_elastic() -> None:
     test_material: list[float] = [2.0, -0.016, 3e-4, 333.0]
     trueStrain: Vector = linspace(0.0, 0.5, 20)
     trueStress: Vector = uniaxial_stress(Yeoh, trueStrain, test_material)
 
-    calibrated_params, calibration_error = single_test_elastic_simplex(
+    calibrated_params, calibration_error = fit_single_test_elastic(
         strain=trueStrain,
         stress=trueStress,
         elastic_model=Yeoh,
@@ -24,4 +25,23 @@ def test_single_test_elastic_simplex() -> None:
     print(f"{calibrated_params=}")
     print(f"{calibration_error=}")
 
-    assert all(isclose(calibrated_params, test_material, rtol=0.05))
+    assert all(isclose(calibrated_params, test_material, rtol=0.1))
+
+
+def test_fit_single_test_incompressible() -> None:
+    test_material: list[float] = [2.0, -0.016, 3e-4]
+    trueStrain: Vector = linspace(0.0, 0.5, 50)
+    trueStress: Vector = uniaxial_stress_incompressible(Yeoh, trueStrain, test_material)
+
+    calibrated_params, calibration_error = fit_single_test_incompressible(
+        strain=trueStrain,
+        stress=trueStress,
+        elastic_model=Yeoh,
+        deformation_mode=uniaxial_stress_incompressible,
+        initial_guess=[1.0, -0.01, 1e-4],
+    )
+
+    print(f"{calibrated_params=}")
+    print(f"{calibration_error=}")
+
+    assert all(isclose(calibrated_params, test_material, rtol=0.01))
